785f21d8ee5e1a4e9ab5cf8d29ff6e54c97da4b2
LCapistrano25 <leonardo.capistrano445@gmail.com>
2025-11-24 11:00:16 -0300

    fix(security): move sensitive settings to environment variables
    Use python-decouple to load SECRET_KEY and DEBUG from environment variables
    Add .env to gitignore to prevent committing sensitive data

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--zxi-a#e*%47i*(=mz06!2t0(yvtj8@ogt9b=ic&b(xc15(kzk'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
